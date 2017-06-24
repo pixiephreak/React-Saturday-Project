04a6550d730f9e17b2fe7a9ad86e5f53869652ae
Jane Claire <pixiephreak@Janes-MacBook-Pro.local>
2017-06-24 13:32:46 -0400

    add JSX

--- app/components/listItem.js
+++ app/components/listItem.js
@@ -3,7 +3,11 @@ import React from "react";
 const ListItem = () => {
   // Rendering an li tag with props.children between the tags (grocery text)
   return (
+    <div>
       <h1>whatever im a profile thing</h1>
+      <h1>whatever im a profile thing</h1>
+    </div>
+
   );
 };
 
