7052f4587efbd941750702c23f77fe340c4dacbf
Jane Claire <pixiephreak@Janes-MacBook-Pro.local>
2017-06-24 11:24:20 -0400

    file structure for compontnets

--- app/App.js
+++ app/App.js
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const appRoot = document.getElementById('app');
-const App = (props) => <p>Hello World</p>;
+const App = (props) => <p>Here is where the container goes</p>;
 
 ReactDOM.render(<App/>, appRoot);
---
+++ app/components/main.js
@@ -0,0 +1,1 @@
+// here is where the header and input box go
